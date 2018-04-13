468509324e50bb96466904b41e149c310dd954b4
Matt <mcranfordmail@gmail.com>
2018-04-13 12:52:12 -0700

    Switch to single quote (js)

--- calc/scripts.js
+++ calc/scripts.js
@@ -1,25 +1,25 @@
 $(function() {
 
-  const $primaryInput = $("#primary-input");
-  const $secondaryInput = $("#secondary-input");
-  const $ac = $("#ac");
-  const $ce = $("#ce");
-  const $divide = $("#divide");
-  const $multiply = $("#multiply");
-  const $minus = $("#minus");
-  const $plus = $("#plus");
-  const $equal = $("#equal");
-  const $decimal = $("#decimal");
-  const $zero = $("#zero");
-  const $one = $("#one");
-  const $two = $("#two");
-  const $three = $("#three");
-  const $four = $("#four");
-  const $five = $("#five");
-  const $six = $("#six");
-  const $seven = $("#seven");
-  const $eight = $("#eight");
-  const $nine = $("#nine");
+  const $primaryInput = $('#primary-input');
+  const $secondaryInput = $('#secondary-input');
+  const $ac = $('#ac');
+  const $ce = $('#ce');
+  const $divide = $('#divide');
+  const $multiply = $('#multiply');
+  const $minus = $('#minus');
+  const $plus = $('#plus');
+  const $equal = $('#equal');
+  const $decimal = $('#decimal');
+  const $zero = $('#zero');
+  const $one = $('#one');
+  const $two = $('#two');
+  const $three = $('#three');
+  const $four = $('#four');
+  const $five = $('#five');
+  const $six = $('#six');
+  const $seven = $('#seven');
+  const $eight = $('#eight');
+  const $nine = $('#nine');
 
 
   // holds numbers for calculation
@@ -31,11 +31,11 @@ $(function() {
     for(let i=0; i<calcArr.length; i++) {
       // console.log(calcArr[i]);
       let input = calcArr[i];
-      if (input === "×") {
-        calcArr[i] = "*";
+      if (input === '×') {
+        calcArr[i] = '*';
       }
-      else if (input === "÷") {
-        calcArr[i] = "/"
+      else if (input === '÷') {
+        calcArr[i] = '/'
       }
       
       
@@ -54,7 +54,7 @@ $(function() {
   // removes last entries made to secondary input
   function clearEntry() {
     for(let i = calcArr.length -1; i>=0; i--) {
-      if(calcArr[i] === "+" || calcArr[i] === "-" || calcArr[i] === "×" || calcArr[i] === "÷") {
+      if(calcArr[i] === '+' || calcArr[i] === '-' || calcArr[i] === '×' || calcArr[i] === '÷') {
         break;
       }
       calcArr.pop();
@@ -66,22 +66,22 @@ $(function() {
     const userVal = event.currentTarget.innerHTML
 	
     // clear calc
-    if (userVal === "AC") {
-      $primaryInput.text("0");
-      $secondaryInput.text("0")
+    if (userVal === 'AC') {
+      $primaryInput.text('0');
+      $secondaryInput.text('0')
       calcArr = [];
 		}
     // clear primary input
-    else if (userVal === "CE") {
-      $primaryInput.text("0");
+    else if (userVal === 'CE') {
+      $primaryInput.text('0');
       clearEntry()
     }
     // operator inputs
-    else if (userVal === "+" || userVal === "-" || userVal === "×" || userVal === "÷" || userVal === "=") {
+    else if (userVal === '+' || userVal === '-' || userVal === '×' || userVal === '÷' || userVal === '=') {
 
-			if ($secondaryInput.text().includes("=")) {
+			if ($secondaryInput.text().includes('=')) {
 
-				if (userVal === "=") {
+				if (userVal === '=') {
 					return false;
 				}
 				$primaryInput.text(userVal);
@@ -90,11 +90,11 @@ $(function() {
 			
 			
       // check that number was passed first
-      else if ($secondaryInput.text() !== "0") {
+      else if ($secondaryInput.text() !== '0') {
 
         // check that an operator isn't already active
-        if ($primaryInput.text() !== "+" && $primaryInput.text() !== "-" && $primaryInput.text() !== "×" && $primaryInput.text() !== "÷") {
-          if(userVal === "=") {
+        if ($primaryInput.text() !== '+' && $primaryInput.text() !== '-' && $primaryInput.text() !== '×' && $primaryInput.text() !== '÷') {
+          if(userVal === '=') {
             let total = calculate(calcArr);
             $primaryInput.text(total);
 						$secondaryInput.append(userVal+total);
@@ -114,7 +114,7 @@ $(function() {
     // handle all number inputs
     else {
 
-			if ($secondaryInput.text().includes("=")) {
+			if ($secondaryInput.text().includes('=')) {
 
 				$primaryInput.text(userVal);
 				$secondaryInput.text(userVal)
@@ -124,15 +124,15 @@ $(function() {
 
 
       // if operator is being used
-      else if ($primaryInput.text() === "+" || $primaryInput.text() === "-" || $primaryInput.text() === "×" || $primaryInput.text() === "÷") {
+      else if ($primaryInput.text() === '+' || $primaryInput.text() === '-' || $primaryInput.text() === '×' || $primaryInput.text() === '÷') {
           $primaryInput.text(userVal);
           $secondaryInput.append(userVal);
           calcArr.push(userVal);
       }
-      else if ($primaryInput.text() === "0") {
+      else if ($primaryInput.text() === '0') {
 
         // check if anything is stored in secondary input
-        if ($secondaryInput.text() === "0") {
+        if ($secondaryInput.text() === '0') {
           $primaryInput.text(userVal);
           $secondaryInput.text(userVal);
           calcArr.push(userVal);
@@ -154,6 +154,6 @@ $(function() {
   }
 
   
-  $(".button").click("click", addInput);
+  $('.button').click('click', addInput);
 
 });
